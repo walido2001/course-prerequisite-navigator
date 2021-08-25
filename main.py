88d60578f6ed657afdbec81b81d169b6cf293cc3
Walid AlDari <78936295+walido2001@users.noreply.github.com>
2021-08-24 21:39:30 -0400

    poosh

--- main.py
+++ main.py
@@ -191,5 +191,7 @@ def server_error(e):
 if __name__ == '__main__':
     # This is used when running locally. Gunicorn is used to run the
     # application on Google App Engine. See entrypoint in app.yaml.
+    app.debug() = True
     app.run(host='127.0.0.1', port=8080, debug=True)
+
 # [END gae_flex_quickstart]
