6d1fc07e10a0396a2fd84fed3eb50c63d3d9dbfd
Walid AlDari <walidald@my.yorku.ca>
2021-09-13 16:14:26 -0400

    Form and Bug page improvements

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 from types import prepare_class
-from flask import Flask, redirect, url_for, render_template
+from flask import Flask, redirect, url_for, render_template, request
 import json
 import logging
-
+import datetime
+import string
 app = Flask(__name__)
 
 
@@ -178,9 +179,38 @@ def mechanical_page_link(course):
 
     return render_template("eng.html", cList=courseCheckList, CourseData=CourseInfo, major="mechanical")
 
-@app.route('/report')
+@app.route('/report', methods=["POST", "GET"])
 def report_page():
-    return render_template("report.html")
+    if request.method == "POST":
+        name = request.form["inputName"]
+        email = request.form["inputEmail"]
+        course = request.form["inputCourse"]
+        eng = request.form["inputType"]
+        description = request.form["inputAddition"]
+        
+        current_time = datetime.datetime.now()
+        date = [str(current_time.year), str(current_time.month), str(current_time.day)]
+        
+        if "" in {name, email, course, eng}:
+            return render_template("report.html", errorStatus = True, successStatus = False) #add error
+        else: 
+            with open('bugsList.json') as json_file:
+                bugs = json.load(json_file)
+
+            bugs.append([name, email, course, eng, date, description])
+
+            with open("bugsList.json", "w") as json_file:
+                json.dump(bugs, json_file)
+
+            return render_template("report.html", errorStatus = False, successStatus = True) #add a thank you for the report
+    return render_template("report.html", errorStatus = False, successStatus = False)
+
+@app.route('/bugs')
+def bug_page():
+    with open('bugsList.json') as json_file:
+        bugs = json.load(json_file)
+    
+    return render_template("bugs.html", bugsList = bugs)
 
 @app.errorhandler(500)
 def server_error(e):
