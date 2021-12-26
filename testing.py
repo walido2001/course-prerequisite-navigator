e11fb2759a820777c34d2535585caf41d0363e7e
Walid AlDari <walidald@my.yorku.ca>
2021-12-25 20:27:55 -0500

    Preparing the project to be deployed to Heroku instead of Google App Engine

--- testing.py
+++
@@ -1,8 +0,0 @@
-import json
-import PrereqScraperFunctions
-
-jfile = open("EngCrs/civilFINAL.json")
-courseList = json.load(jfile)
-
-with open('EngCrs/civilFINALTEST.json', 'w') as json_file:
-    json.dump(PrereqScraperFunctions.clean_final(courseList), json_file)
